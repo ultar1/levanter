c98de49b5b4b169f1b86293899f43925bd38b257
ultar1 <Obelenwaluke2004@gmail.com>
2025-02-16 17:21:33 +0100

    yh

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, logger } = require('./lib/client');
 const { DATABASE, VERSION } = require('./config');
-const { stopInstance } = require('./lib/pm2');
 
 const start = async () => {
   logger.info(`levanter ${VERSION}`);
