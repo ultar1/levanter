dd45fbcf46747ba8fb9fd0b975262a70e3134172
ultar1 <Obelenwaluke2004@gmail.com>
2025-02-16 17:02:45 +0100

    yh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,8 @@ module.exports = {
       name: 'levanter',
       script: 'npm',
       args: 'start',
-      cron_restart: '0 0 * * *', // Example cron pattern to restart the app daily at midnight
+      // Remove cron_restart if it's causing issues
+      // cron_restart: '0 0 * * *', // Example cron pattern to restart the app daily at midnight
       watch: true,
       env: {
         NODE_ENV: 'production'
