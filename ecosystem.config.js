c1682995b1a0133ee845ca26dd5dc3f3f810f188
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-02-23 01:14:45 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,12 @@ module.exports = {
     wait_ready: true,
     exp_backoff_restart_delay: 100,
     env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      SESSION_ID: process.env.SESSION_ID || '',
+      HEROKU_API_KEY: process.env.HEROKU_API_KEY || '',
+      HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || '',
+      PREFIX: process.env.PREFIX || '.',
+      SUDO: process.env.SUDO || ''
     }
   }]
 }
