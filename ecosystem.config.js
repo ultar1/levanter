342abd84853b4d67f26b8fd44ebd2faf6d043f1b
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-02-23 11:02:16 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,30 @@
-module.exports = {
-  apps: [
-    {
-      name: 'hermit-md',
-      script: 'index.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '450M',
-    },
-  ],
+const { Client, logger } = require('./lib/client');
+const { DATABASE, VERSION } = require('./config');
+const { stopInstance } = require('./lib/pm2');
+
+const keepRunning = () => {
+  logger.info(`levanter ${VERSION}`);
+  const start = async () => {
+    try {
+      await DATABASE.authenticate({ retry: { max: 3 } });
+    } catch (error) {
+      const databaseUrl = process.env.DATABASE_URL;
+      logger.error({ msg: 'Unable to connect to the database', error: error.message, databaseUrl });
+      return stopInstance();
+    }
+    try {
+      const bot = new Client();
+      await bot.connect();
+    } catch (error) {
+      logger.error(error);
+    }
+  };
+
+  start();
+
+  setInterval(() => {
+    logger.info('Worker is still running...');
+  }, 60000);
 };
+
+keepRunning();
