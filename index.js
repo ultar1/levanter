385501ed45504410f327dca8f366e78bec81e5c5
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-02-23 23:12:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,27 @@
-const { spawnSync, spawn } = require('child_process');
-const { existsSync, writeFileSync } = require('fs');
-const path = require('path');
-const config = require('./config.json');
-const { Client } = require('./lib/client');
-const { getVars } = require('./lib/vars');
+const { spawnSync, spawn } = require('child_process')
+const { existsSync, writeFileSync } = require('fs')
+const path = require('path')
 
-const SESSION_ID = process.env.SESSION_ID || config.SESSION_ID;
-if (!SESSION_ID) {
-  throw new Error('SESSION_ID is not defined');
-}
+const SESSION_ID = 'levanter_24b2cf2a83f6df4f07a1a579db54d32a94' // Edit this line only, don't remove ' <- this symbol
 
-let nodeRestartCount = 0;
-const maxNodeRestarts = 5;
-const restartWindow = 30000; // 30 seconds
-let lastRestartTime = Date.now();
-
-function startPm2() {
-  const pm2 = spawn('yarn', ['pm2', 'start', 'index.js', '--name', 'levanter', '--attach'], {
-    cwd: 'levanter',
-    stdio: ['pipe', 'pipe', 'pipe'],
-  });
-
-  let restartCount = 0;
-  const maxRestarts = 5; // Adjust this value as needed
-
-  pm2.on('exit', (code) => {
-    if (code !== 0) {
-      // console.log('yarn pm2 failed to start, falling back to node...')
-      startNode();
-    }
-  });
-
-  pm2.on('error', (error) => {
-    console.error(`yarn pm2 error: ${error.message}`);
-    startNode();
-  });
-
-  // Check for infinite restarts
-  if (pm2.stderr) {
-    pm2.stderr.on('data', (data) => {
-      const output = data.toString();
-      if (output.includes('restart')) {
-        restartCount++;
-        if (restartCount > maxRestarts) {
-          // console.log('yarn pm2 is restarting indefinitely, stopping yarn pm2 and starting node...')
-          spawnSync('yarn', ['pm2', 'delete', 'levanter'], { cwd: 'levanter', stdio: 'inherit' });
-          startNode();
-        }
-      }
-    });
-  }
-
-  if (pm2.stdout) {
-    pm2.stdout.on('data', (data) => {
-      const output = data.toString();
-      console.log(output);
-      if (output.includes('Connecting')) {
-        // console.log('Application is online.')
-        restartCount = 0;
-      }
-    });
-  }
-}
-
-const startNode = async () => {
+let nodeRestartCount = 0
+const maxNodeRestarts = 5
+const restartWindow = 30000 // 30 seconds
+let lastRestartTime = Date.now()
+const start = async () => {
+  logger.info(`levanter ${VERSION}`)
   try {
-    const client = new Client();
-    await client.connect(SESSION_ID);
-    logger.info('Client connected');
-  } catch (err) {
-    logger.error(err.message, { err });
-    process.exit(1);
+    await DATABASE.authenticate({ retry: { max: 3 } })
+  } catch (error) {
+    const databaseUrl = process.env.DATABASE_URL
+    logger.error({ msg: 'Unable to connect to the database', error: error.message, databaseUrl })
+    return stopInstance()
   }
-};
-
-startPm2();
+  try {
+    const bot = new Client()
+    await bot.connect()
+  } catch (error) {
+    logger.error(error)
+  }
+}
+start()
