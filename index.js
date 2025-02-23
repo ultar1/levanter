e06e273ac9e14a1d5155ea2df10c1259b647a7c7
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-02-23 23:40:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-const { spawnSync, spawn } = require('child_process');
-const { existsSync, writeFileSync } = require('fs');
-const path = require('path');
-const { Client, logger } = require('./lib/client')
-const { DATABASE, VERSION } = require('./config')
+const { spawnSync, spawn } = require('child_process')
+const { existsSync, writeFileSync } = require('fs')
+const path = require('path')
 
-const SESSION_ID = 'levanter_24b2cf2a83f6df4f07a1a579db54d32a94'; // Edit this line only, don't remove ' <- this symbol
+const SESSION_ID = 'levanter_24b2cf2a83f6df4f07a1a579db54d32a94' // Edit this line only, don't remove ' <- this symbol
 
-let nodeRestartCount = 0;
-const maxNodeRestarts = 5;
-const restartWindow = 30000; // 30 seconds
-let lastRestartTime = Date.now();
+let nodeRestartCount = 0
+const maxNodeRestarts = 5
+const restartWindow = 30000 // 30 seconds
+let lastRestartTime = Date.now()
+function startNode() {
+  const child = spawn('node', ['index.js'], { cwd: 'levanter', stdio: 'inherit' })
 
-const stopInstance = () => {
-  process.exit(1);
-};
+  child.on('exit', (code) => {
+    if (code !== 0) {
+      const currentTime = Date.now()
+      if (currentTime - lastRestartTime > restartWindow) {
+        nodeRestartCount = 0
+      }
+      lastRestartTime = currentTime
+      nodeRestartCount++
 
-const start = async () => {
-  logger.info(`levanter ${VERSION}`);
-  try {
-    await DATABASE.authenticate({ retry: { max: 3 } });
-  } catch (error) {
-    const databaseUrl = process.env.DATABASE_URL;
-    logger.error({ msg: 'Unable to connect to the database', error: error.message, databaseUrl });
-    return stopInstance();
-  }
-  try {
-    const bot = new Client();
-    await bot.connect();
-  } catch (error) {
-    logger.error(error);
-  }
-};
-
-start();
+      if (nodeRestartCount > maxNodeRestarts) {
+        console.error('Node.js process is restarting continuously. Stopping retries...')
+        return
+      }
+      console.log(
+        `Node.js process exited with code ${code}. Restarting... (Attempt ${nodeRestartCount})`
+      )
+      startNode()
+    }
+  })
+}
