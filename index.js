9d768941ffb8e8c88aae6c1a83e467a02d80f464
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-02-24 00:19:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,26 +8,50 @@ let nodeRestartCount = 0
 const maxNodeRestarts = 5
 const restartWindow = 30000 // 30 seconds
 let lastRestartTime = Date.now()
-function startNode() {
-  const child = spawn('node', ['index.js'], { cwd: 'levanter', stdio: 'inherit' })
+function startPm2() {
+  const pm2 = spawn('yarn', ['pm2', 'start', 'index.js', '--name', 'levanter', '--attach'], {
+    cwd: 'levanter',
+    stdio: ['pipe', 'pipe', 'pipe'],
+  })
 
-  child.on('exit', (code) => {
+  let restartCount = 0
+  const maxRestarts = 5 // Adjust this value as needed
+
+  pm2.on('exit', (code) => {
     if (code !== 0) {
-      const currentTime = Date.now()
-      if (currentTime - lastRestartTime > restartWindow) {
-        nodeRestartCount = 0
-      }
-      lastRestartTime = currentTime
-      nodeRestartCount++
-
-      if (nodeRestartCount > maxNodeRestarts) {
-        console.error('Node.js process is restarting continuously. Stopping retries...')
-        return
-      }
-      console.log(
-        `Node.js process exited with code ${code}. Restarting... (Attempt ${nodeRestartCount})`
-      )
+      // console.log('yarn pm2 failed to start, falling back to node...')
       startNode()
     }
   })
+
+  pm2.on('error', (error) => {
+    console.error(`yarn pm2 error: ${error.message}`)
+    startNode()
+  })
+
+  // Check for infinite restarts
+  if (pm2.stderr) {
+    pm2.stderr.on('data', (data) => {
+      const output = data.toString()
+      if (output.includes('restart')) {
+        restartCount++
+        if (restartCount > maxRestarts) {
+          // console.log('yarn pm2 is restarting indefinitely, stopping yarn pm2 and starting node...')
+          spawnSync('yarn', ['pm2', 'delete', 'levanter'], { cwd: 'levanter', stdio: 'inherit' })
+          startNode()
+        }
+      }
+    })
+  }
+
+  if (pm2.stdout) {
+    pm2.stdout.on('data', (data) => {
+      const output = data.toString()
+      console.log(output)
+      if (output.includes('Connecting')) {
+        // console.log('Application is online.')
+        restartCount = 0
+      }
+    })
+  }
 }
