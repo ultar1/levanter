ea8175c0e607bb7c06e076924371320f8bf4c5f1
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-02-24 01:34:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+const { spawn } = require('child_process');
+
 function startPm2() {
   const pm2 = spawn('yarn', ['pm2', 'start', 'index.js', '--name', 'levanter', '--attach'], {
     cwd: 'levanter',
     stdio: ['pipe', 'pipe', 'pipe'],
-  })
+  });
 
-  let restartCount = 0
-  const maxRestarts = 5 // Adjust this value as needed
+  let restartCount = 0;
+  const maxRestarts = 5; // Adjust this value as needed
 
   pm2.on('exit', (code) => {
-startpm2()
- })
+    if (restartCount < maxRestarts) {
+      console.log(`Process exited with code ${code}. Restarting...`);
+      restartCount++;
+      setTimeout(startPm2, 1000); // Adding a delay of 1 second before restarting
+    } else {
+      console.log('Max restarts reached. Exiting...');
+    }
+  });
+}
+
+startPm2();
