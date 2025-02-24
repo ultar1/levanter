30b0acf195c2ec8ca4919595cc0e1eaf7053fce2
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-02-24 01:28:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const client = require('./lib/client')
+function startPm2() {
+  const pm2 = spawn('yarn', ['pm2', 'start', 'index.js', '--name', 'levanter', '--attach'], {
+    cwd: 'levanter',
+    stdio: ['pipe', 'pipe', 'pipe'],
+  })
 
-const connect = async () => {
-	try {
-		await client.connect()
-	} catch (error) {
-		console.error(error)
-	}
-}
+  let restartCount = 0
+  const maxRestarts = 5 // Adjust this value as needed
 
-connect()
+  pm2.on('exit', (code) => {
+startpm2()
+ })
