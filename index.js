5a3ac709b946852b798fc9aed2d6e8e0369caa9f
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-03-09 18:21:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const client = require('./lib/client')
+const { Client, logger } = require('./lib/client')
+const { DATABASE, VERSION } = require('./config')
+const { stopInstance } = require('./lib/pm2')
 
-const connect = async () => {
-	try {
-		await client.connect()
-	} catch (error) {
-		console.error(error)
-	}
+const start = async () => {
+  logger.info(`levanter ${VERSION}`)
+  try {
+    await DATABASE.authenticate({ retry: { max: 3 } })
+  } catch (error) {
+    const databaseUrl = process.env.DATABASE_URL
+    logger.error({ msg: 'Unable to connect to the database', error: error.message, databaseUrl })
+    return stopInstance()
+  }
+  try {
+    const bot = new Client()
+    await bot.connect()
+  } catch (error) {
+    logger.error(error)
+  }
 }
-
-connect()
+start()
